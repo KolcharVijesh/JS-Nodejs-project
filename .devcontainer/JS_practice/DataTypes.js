d88b54bb63cc171fca51528ef12f38a08a43f731
KolcharVijesh <162454002+KolcharVijesh@users.noreply.github.com>
2024-03-19 16:30:03 +0000

    Datatypes

---
+++ .devcontainer/JS_practice/DataTypes.js
@@ -0,0 +1,14 @@
+//number =>2 to power 53
+//bigint
+//String => " name"
+//boolean => true/false
+//null => standalone value
+// undefined
+//symbol =>unique
+
+let name ="Vijesh"
+let age = 26
+let number= null
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
