87ff509511a9a8f5e452fb89aa0b95e6b2d629f2
KolcharVijesh <162454002+KolcharVijesh@users.noreply.github.com>
2024-03-21 16:20:36 +0000

    datattype update

--- .devcontainer/JS_practice/DataTypes.js
+++ .devcontainer/JS_practice/DataTypes.js
@@ -1,3 +1,9 @@
+//Primitive data types
+// 7 types: String ,Number ,Boolean, null,undefined,symbol,BigInt
+//Reference data types (non primitive)
+//   Array,Objects,Functions
+
+
 //number =>2 to power 53
 //bigint
 //String => " name"
